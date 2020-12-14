d6e8c85fac33b49293011addb329e6200182b889
jannejantti <janne.jantti@edu.salpaus.fi>
2020-12-14 09:06:19 +0000

    20201214

--- scriptit/portfolioscript.js
+++ scriptit/portfolioscript.js
@@ -1,24 +1,24 @@
-//muuttujat
-const nimi = 'Janne Jantti'; //string
-const ika = 16; //integer, eli kokonaisluku
-const onkoopiskelija = true; //boolean
+
+const nimi = 'Janne Jantti'; 
+const ika = 16; 
+const onkoopiskelija = true; 
 
 console.log(nimi);
 console.log(ika);
 console.log('Onko opiskelija:' + ' ' + onkoopiskelija);
-//muuttujat loppuu
 
-//ehtolause
+
+
 const opiskelija = onkoopiskelija === true ? 'opiskelija' : 'ammattilainen';
 console.log(opiskelija);
-//ehtolause loppuu
 
-//heisivuillenisurffaaja
+
+
 const surffaaja = `Hei sivuilleni surffaaja, olen ${nimi} ja olen ${ika}-vuotias ${opiskelija}`;
 console.log(surffaaja);
-//loppuu
 
-//array
+
+
 var henkilo1 = "1.ystäväni: Timo Nieminen";
 var henkilo2 = "2.ystäväni: Matti Korhonen";
 var henkilo3 = "3.ystäväni: Kari Koskinen";
@@ -28,9 +28,7 @@ console.log(henkilo1.split(' '));
 console.log(henkilo2.split(' '));
 console.log(henkilo3.split(' '));
 console.log(henkilo4.split(' '));
-//array loppuu
 
-//javascript objekti alkaa
 const perhe = [
   {
     id: 1,
@@ -63,43 +61,31 @@ const perhe = [
     taysiikainen: false
   }];
 console.log(perhe);
-//javascript objekti loppuu
 
-//mappaus
 const perhe1Mappaus = perhe.map(function (perhe1) {
   return perhe1;
 });
 console.log(perhe1Mappaus);
-//mappaus loppuu
 
-//taysiikaiset
 const perhe2Filter = perhe.filter(function (perhe2) {
   return perhe2.taysiikainen === true;
 }).map(function (perhe2) {
   return perhe2.etunimi;
 });
 console.log(perhe2Filter);
-//taysiikaiset loppuu
 
-//alaikaiset
 const perhe3Filter = perhe.filter(function (perhe3) {
   return perhe3.taysiikainen === false;
 }).map(function (perhe3) {
   return perhe3.etunimi;
 });
 console.log(perhe3Filter);
-//alaikaiset loppuu
 
-//funktionkutsu
 function kaikkiiat(ekaNumero, tokaNumero, kolmasNumero, neljasNumero) {
   return ekaNumero + tokaNumero + kolmasNumero + neljasNumero;
 }
 console.log(kaikkiiat(42, 40, 21, 18));
-//funktionkutsu loppuu
 
-/*
-17.11 Asiat, yhteydenottosivun muutokset
-*/
 const nimiSisalto = document.querySelector('#nimi');
 const sukunSisalto = document.querySelector('#sukunimi');
 const sahkoSisalto = document.querySelector('#sahkoposti');
@@ -211,9 +197,9 @@ function sendJSON() {
   const email = document.querySelector('#sahkoposti').value;
 
   var data = JSON.stringify({
-    "EmailMsg": "Sähköpostin lähetys toimi!", //kirjoittaa sähköpostin sisällön
-    "EmailTo": "janne.jantti", //oma sähköposti
-    "EmailName": "Teppo Tyyppi" //nimi-kentän sisältö
+    "EmailMsg": "Sähköpostin lähetys toimi!", 
+    "EmailTo": "janne.jantti", 
+    "EmailName": "Teppo Tyyppi" 
   });
   xhr.send(data);
 }
